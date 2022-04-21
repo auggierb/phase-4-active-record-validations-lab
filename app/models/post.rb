6f501c0fac125c822518dd99aa12ce908c05ae5d
August <auggierutter@gmail.com>
2022-04-21 16:33:28 -0400

    commit tree 07c5c20602b518a21bee61998fa608fc66aac598

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :customVal, on: :create
+   
+    def customVal
+        errors.add("no") unless !title.include?("yahg")
+ 
+    end
+
 end
